68d0b54c95917e4ebb0a32c29219ed6b10cfe632
teampiaf <piaf.contact@gmail.com>
2026-01-23 23:29:45 +0100

    Fix : Restructuration

--- piaf-site-main/src/config/links.ts
+++ piaf-site-main/src/config/links.ts
@@ -1,5 +1,11 @@
 export const LINKS = {
+  home: "/",
+  howItWorks: "/comment-ca-marche",
+  project: "/decouvrir-le-projet",
+  download: "/download",
+
+  // externes
   instagram: "https://www.instagram.com/team_piaf/",
-  linkedin: "https://www.linkedin.com/in/team-piaf",
+  linkedin: "https://www.linkedin.com/in/team-piaf/",
   tiktok: "https://www.tiktok.com/@fly_with_piaf",
-};
+} as const;
